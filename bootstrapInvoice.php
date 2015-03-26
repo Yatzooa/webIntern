8dca8360705a8844cecb5ad35a11e9346c76af72
jhoang <j.hoang25@gmail.com>
2015-03-25 23:24:42 -0700

    adding bootstrap file, changed filepath for all pages to local

--- bootstrapInvoice.php
+++ bootstrapInvoice.php
@@ -24,7 +24,7 @@ $price=count($_POST['price']) ? $_POST['price'] :array();
 		}
 		</style>
 		<title>Invoice</title>
-		<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="bootstrap-3.3.4-dist/css/bootstrap.min.css">
 	  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 		<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 	</head>
